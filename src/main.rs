4f517379e19db8660deb19f505c0d4252186cdf8
Web3js 中文网 <118263377+web3js-cn@users.noreply.github.com>
2022-12-13 20:05:50 +0800

    读取 chunk 到 Chunk 结构体, 包含了头部、主函数 upvalues、主函数原型

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 pub mod header;
+pub mod chunk;
 
 fn main() {
     // 从 chunk 中读取头部 Header 并检查 check()
-    let header = header::Header::new("src/luac.out");
-    header.check();
-    println!("{}", header);
+    // let header = header::Header::new("src/luac.out");
+    // header.check();
+    // println!("{}", header);
+
+
+    // 读取 chunk 到 Chunk 结构体, 包含了头部、主函数 upvalues、主函数原型
+    let mut chunk = chunk::Chunk::new("src/luac.out");
+    println!("{:#?}", chunk);
 }
