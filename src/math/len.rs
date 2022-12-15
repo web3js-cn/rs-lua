47ab61bd2b234644a6bff4509fd90804de14f498
Web3js 中文网 <118263377+web3js-cn@users.noreply.github.com>
2022-12-15 12:36:59 +0800

    实现 Len() 获取长度 访问指定索引处的值, 取其长度, 然后推入栈顶

---
+++ src/math/len.rs
@@ -0,0 +1,16 @@
+//! 实现 Len() 获取长度 访问指定索引处的值, 取其长度, 然后推入栈顶
+
+use crate::lua_stack::luaValue;
+use crate::lua_state::luaState;
+
+impl luaState {
+    pub fn Len(&mut self, idx: i64) {
+        let val = self.statck.get(idx).unwrap();
+        match val {
+            luaValue::Str(x) => {
+                self.statck.push(luaValue::I64(x.len() as i64))
+            },
+            _ => panic!("length error")
+        }
+    }
+}
