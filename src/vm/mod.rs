42dd29c333fee508e7827466262af0df5cbf98eb
Web3js 中文网 <118263377+web3js-cn@users.noreply.github.com>
2022-12-16 18:01:50 +0800

    漏了 Integer 的情况

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -11,16 +11,17 @@ use crate::lua_stack::luaValue;
 use crate::lua_state::luaState;
 
 impl luaState {
-    fn PC(&mut self) -> i64 {
+    pub(crate) fn PC(&mut self) -> i64 {
         self.pc
     }
 
     fn AddPC(&mut self, n: i64) {
-        self.pc += 1;
+        self.pc += n;
     }
 
     /// 根据索引从函数原型的指令表里取出当前指令, 然后递增 pc
-    fn Fetch(&mut self) -> u32 {
+    pub(crate) fn Fetch(&mut self) -> u32 {
+        // print!("Fetch Pc={} ", self.pc);
         let i = self.proto.code[self.pc as usize];
         self.pc += 1;
         i
@@ -30,6 +31,7 @@ impl luaState {
     /// 这个方法可能会有问题 需要留意
     fn GetConst(&mut self, idx: i64) {
         let val = self.proto.constants.get(idx as usize);
+        // println!("\nGetConst 调用: {:?}", val.unwrap());
         if let Constant::TAG_LONG_STR(s) = val.unwrap() {
             self.statck.push(luaValue::Str(s.clone().to_owned()));
         }
@@ -49,6 +51,9 @@ impl luaState {
         if let Constant::TAG_NUMBER(i) = val.unwrap() {
             self.statck.push(luaValue::I64(i.to_owned() as i64));
         }
+        if let Constant::TAG_INTEGER(int) = val.unwrap() {
+            self.statck.push(luaValue::I64(int.to_owned() as i64));
+        }
     }
 
     /// 调用 GetConst 将某个常量推入栈顶 或者调用 PushValue 将某个索引处的栈值推入栈顶
@@ -58,6 +63,7 @@ impl luaState {
             self.GetConst(rk & 0xFF);
         } else {
             // register
+            // println!("getrk rk={}", rk);
             self.PushValue(rk+1);
         }
     }
